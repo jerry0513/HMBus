df8fbe3573fa88979933807bda352dbe24e0dc62
HungMing <zxcv860513@gmail.com>
2021-07-04 15:06:50 +0800

    fix lazy lifecycle in BaseFragment

--- app/src/main/java/tw/com/hmbus/ui/BaseFragment.kt
+++ app/src/main/java/tw/com/hmbus/ui/BaseFragment.kt
@@ -2,9 +2,11 @@ package tw.com.hmbus.ui
 
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.lifecycleScope
 
 abstract class BaseFragment(@LayoutRes contentLayoutId: Int) : Fragment(contentLayoutId) {
-    val fragmentScope = viewLifecycleOwner.lifecycleScope
-    val viewLifecycle = viewLifecycleOwner.lifecycle
+    val fragmentScope: LifecycleCoroutineScope by lazy { viewLifecycleOwner.lifecycleScope }
+    val viewLifecycle: Lifecycle by lazy { viewLifecycleOwner.lifecycle }
 }
